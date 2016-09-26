c5d85cb9d33539d8227f0b1d37fce84f57891ce0
nguyen <nguyen@xuanthanh>
2016-09-27 01:46:57 +0700

    test commit

---
+++ demoGithub/src/demoGithub/Main.java
@@ -0,0 +1,18 @@
+package demoGithub;
+
+public class Main {
+	public static void main(String[] args){
+		data Data= new data();
+		
+		displayGreeting();
+		Data.display();
+	
+	}
+	private static void displayGreeting(){
+		System.out.println("Hello mn");
+		System.out.println("Xin chao");
+		System.out.println("Tam biet");
+		
+	}
+
+}
---
+++ demoGithub/src/demoGithub/data.java
@@ -0,0 +1,11 @@
+package demoGithub;
+
+public class data {
+	int data[] = {0,4,42};
+	
+	public void display(){
+		System.out.printf("TODO : implement data display");
+	}
+	
+
+}
